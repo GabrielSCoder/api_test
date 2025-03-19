218e4b798ee5987ce6cc3dc0567d6bfaa816f12f
Gabriel Sena <gabrielsenasaraiva7@gmail.com>
2025-03-19 19:33:49 -0300

    x

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,12 @@ const app = express()
 
 app.use(express.json());
 
-app.get("/meet", (req, res) => {
+app.get("/", (req, res) => {
     res.status(200).json({success : true, message: "Olá!"})
+})
+
+app.get("/meet", (req, res) => {
+    res.status(200).json({success : true, message: "Olá, tudo bem?"})
 })
 
 app.listen(123, () => console.log("Server ready on port 3000."));
